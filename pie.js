49f87f1e52dbc3946370f65369672b1c2c0f3ba3
Hernan Y.Ke <yuriqiao@gmail.com>
2015-08-16 13:34:42 -0400

    Basic piechart

---
+++ pie.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Hernan Y.Ke on 2015/8/16.
+ */
+(function(){
+    var svg = d3.select("#graph").append("svg").style({
+        width:"100%",
+        height:500
+    });
+
+    var manually = function(data,x,y){
+        var colors =d3.scale.category20c(),
+            total = d3.sum(data.map(function(d) {return d.value;})),
+            offset = function(d,i){
+                return d3.sum(data.slice(0,i).map(function(d){
+                    return 2*Math.PI * (d.value/total);
+                }));
+            },
+            arc = d3.svg.arc().outerRadius(150).startAngle(offset).endAngle(function(d,i){
+                return offset(d,i)+2*Math.PI*(d.value/total);
+            }),
+            slice = svg.selectAll('.slice').data(data).enter().append('g').attr("transform","translate(" +x+ "," +y+")");
+
+        slice.append("path").attr({d:arc, fill:function(d,i){
+            return colors(i);
+        },title:function(d){
+            return d.label + " (" + d.value + ")";
+        }});
+    };
+    d3.json("ufo-types.json",function(data){
+        manually(data, 300, 250);
+    })
+})();
